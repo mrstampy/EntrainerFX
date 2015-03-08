36bb1a2102c6ca4e2d5dfe228e24b96eb744618d
burton <burt.alexander@gmail.com>
2015-03-09 09:25:36 +1000

    helps to commit the new superclass

---
+++ EntrainerFX/src/main/java/net/sourceforge/entrainer/gui/jfx/AbstractTitledPane.java
@@ -0,0 +1,69 @@
+package net.sourceforge.entrainer.gui.jfx;
+
+import net.sourceforge.entrainer.mediator.EntrainerMediator;
+import net.sourceforge.entrainer.mediator.MediatorConstants;
+import net.sourceforge.entrainer.mediator.ReceiverChangeEvent;
+import net.sourceforge.entrainer.mediator.Sender;
+import net.sourceforge.entrainer.mediator.SenderAdapter;
+import javafx.scene.Node;
+import javafx.scene.control.Labeled;
+import javafx.scene.control.TitledPane;
+import javafx.scene.paint.Color;
+
+public abstract class AbstractTitledPane extends TitledPane {
+
+	private static final Color TEXT_FILL = Color.CORNSILK;
+
+	private Sender sender = new SenderAdapter();
+
+	public AbstractTitledPane(String title) {
+		super();
+		setText(title);
+		setStyle("-fx-background-color: black");
+		EntrainerMediator.getInstance().addSender(sender);
+	}
+
+	/**
+	 * Clear mediator objects.
+	 */
+	public void clearMediatorObjects() {
+		EntrainerMediator.getInstance().removeReceiver(this);
+		EntrainerMediator.getInstance().removeSender(sender);
+	}
+
+	protected void init() {
+		setStyle("-fx-background-color: black");
+		Node contentPane = getContentPane();
+		contentPane.setStyle("-fx-background-color: black");
+		setContent(contentPane);
+
+		expandedProperty().addListener(e -> setOpacity(isExpanded() ? 0.75 : 0.25));
+	}
+	
+	protected abstract Node getContentPane();
+	
+	protected void setTextFill(Labeled lb) {
+		lb.setTextFill(TEXT_FILL);
+	}
+
+	protected void fireReceiverChangeEvent(double value, MediatorConstants parm) {
+		sender.fireReceiverChangeEvent(new ReceiverChangeEvent(this, value, parm));
+	}
+
+	protected void fireReceiverChangeEvent(boolean value, MediatorConstants parm) {
+		sender.fireReceiverChangeEvent(new ReceiverChangeEvent(this, value, parm));
+	}
+
+	protected void fireReceiverChangeEvent(String value, MediatorConstants parm) {
+		sender.fireReceiverChangeEvent(new ReceiverChangeEvent(this, value, parm));
+	}
+
+	protected void fireReceiverChangeEvent(int value, MediatorConstants parm) {
+		sender.fireReceiverChangeEvent(new ReceiverChangeEvent(this, value, parm));
+	}
+
+	protected void fireReceiverChangeEvent(java.awt.Color value, MediatorConstants parm) {
+		sender.fireReceiverChangeEvent(new ReceiverChangeEvent(this, value, parm));
+	}
+
+}
