32fd975b5fc11a3676474b0c4320bfbc90416262
burton <burt.alexander@gmail.com>
2015-03-19 08:26:18 +1000

    version

--- EntrainerFX/src/main/java/net/sourceforge/entrainer/Version.java
+++ EntrainerFX/src/main/java/net/sourceforge/entrainer/Version.java
@@ -25,7 +25,7 @@ package net.sourceforge.entrainer;
 public interface Version {
 
 	/** The Constant VERSION. */
-	public static final String VERSION = "5.2.0";
+	public static final String VERSION = "6.0.0";
 
 	/** The Constant RELEASE. */
 	public static final String RELEASE = "???, 2015";
