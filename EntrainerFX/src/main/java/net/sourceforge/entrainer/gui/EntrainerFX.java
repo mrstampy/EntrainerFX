aeb3c3ff20fffabfd9327d61aef002f0a286c9c8
burton <burt.alexander@gmail.com>
2015-03-20 22:37:56 +1000

    titledpane reorder

--- EntrainerFX/src/main/java/net/sourceforge/entrainer/gui/EntrainerFX.java
+++ EntrainerFX/src/main/java/net/sourceforge/entrainer/gui/EntrainerFX.java
@@ -1657,11 +1657,11 @@ public class EntrainerFX extends JFrame implements EntrainerResources {
 		int v = 0;
 		GridPane.setConstraints(sliderControlPane, 0, v++);
 		GridPane.setMargin(sliderControlPane, new Insets(20, 0, 0, 0));
+		GridPane.setConstraints(audioPlayerPane, 0, v++);
 		GridPane.setConstraints(pictures, 0, v++);
 		GridPane.setConstraints(flashOptions, 0, v++);
-		GridPane.setConstraints(animations, 0, v++);
 		GridPane.setConstraints(shimmerOptions, 0, v++);
-		GridPane.setConstraints(audioPlayerPane, 0, v++);
+		GridPane.setConstraints(animations, 0, v++);
 		GridPane.setConstraints(neuralizer, 0, v++);
 
 		gp.setPadding(new Insets(5, 13, 5, 5));
